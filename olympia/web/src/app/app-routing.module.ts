389840bcf2059a2cbe3a93b33f8842cc0d8e7965
Robert <robert.goepfert1999@gmail.com>
2023-10-15 16:43:26 +0200

    edit: Routing mit Platzhaltern hinzugefügt

--- olympia/web/src/app/app-routing.module.ts
+++ olympia/web/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LandingPageComponent } from "./components/landing-page/landing-page.component";
+import { LoginComponent } from "./components/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: "", title: "Olympia", component: LandingPageComponent },
+	{ path: "login", title: "Login", component: LoginComponent },
+
+	// Platzhalter:
+	{ path: "laender", title: "Länder", component: LandingPageComponent },
+	{ path: "ergebnisse", title: "Ergebnisse", component: LandingPageComponent },
+	{
+		path: "medaillenspiegel",
+		title: "Medaillenspiegel",
+		component: LandingPageComponent,
+	},
+	{ path: "admin", title: "Admin", component: LandingPageComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
