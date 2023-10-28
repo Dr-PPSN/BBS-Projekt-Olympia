8ba28f1e3b2a51e5a875394df3e548d1772623fd
Robert <robert.goepfert1999@gmail.com>
2023-10-28 05:29:16 +0200

    add: Passwort zurücksetzen Formular hinzugefügt

--- olympia/web/src/app/pages/login/reset-password/reset-password.component.ts
+++ olympia/web/src/app/pages/login/reset-password/reset-password.component.ts
@@ -1,8 +1,19 @@
 import { Component } from "@angular/core";
+import { Formular } from "../login.constant";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
 	selector: "app-reset-password",
 	templateUrl: "./reset-password.component.html",
 	styleUrls: ["./reset-password.component.sass"],
 })
-export class ResetPasswordComponent {}
+export class ResetPasswordComponent {
+	public Formular = Formular;
+	public formular = new FormGroup({
+		email: new FormControl("", [Validators.required, Validators.email]),
+	});
+
+	public resetPassword() {
+		console.log(this.formular.value);
+	}
+}
