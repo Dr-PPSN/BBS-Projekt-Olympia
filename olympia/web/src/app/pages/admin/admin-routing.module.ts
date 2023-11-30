ad141cbee6681fa45396ae4b340ec87e2badfbd3
Robert <robert.goepfert1999@gmail.com>
2023-11-30 10:10:37 +0100

    fix: Admin Routing

--- olympia/web/src/app/pages/admin/admin-routing.module.ts
+++ olympia/web/src/app/pages/admin/admin-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AthletenComponent } from "./admin-uebersicht/athleten/athleten.component";
 import { KampfrichterComponent } from "./admin-uebersicht/kampfrichter/kampfrichter.component";
+import { AdminUebersichtComponent } from "./admin-uebersicht/admin-uebersicht.component";
 
 const routes: Routes = [
+	{
+		path: "",
+		title: "",
+		component: AdminUebersichtComponent,
+	},
 	{
 		path: "athleten",
 		title: "Athleten",
@@ -13,11 +19,6 @@ const routes: Routes = [
 		path: "kampfrichter",
 		title: "Kampfrichter",
 		component: KampfrichterComponent,
-	},
-	{
-		path: "",
-		redirectTo: "athleten",
-		pathMatch: "full",
 	},
 ];
 
