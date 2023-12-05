55b5d1077706edfc1da8fe4e2e29fd91b17d1ef1
Robert <robert.goepfert1999@gmail.com>
2023-12-05 13:33:42 +0100

    add: Länderübersicht Test

--- olympia/web/src/app/pages/laender/laender-uebersicht/laender-uebersicht.component.ts
+++ olympia/web/src/app/pages/laender/laender-uebersicht/laender-uebersicht.component.ts
@@ -1,8 +1,18 @@
 import { Component } from "@angular/core";
+import { ChartType } from "angular-google-charts";
 
 @Component({
 	selector: "laender-uebersicht",
 	templateUrl: "./laender-uebersicht.component.html",
 	styleUrls: ["./laender-uebersicht.component.sass"],
 })
-export class LaenderUebersichtComponent {}
+export class LaenderUebersichtComponent {
+	public ChartType = ChartType;
+	public chartData = [
+		["London", 8136000],
+		["New York", 8538000],
+		["Paris", 2244000],
+		["Berlin", 3470000],
+		["Kairo", 19500000],
+	];
+}
--- olympia/web/src/app/pages/laender/laender.module.ts
+++ olympia/web/src/app/pages/laender/laender.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { LaenderRoutingModule } from "./laender-routing.module";
+import { GoogleChartsModule } from "angular-google-charts";
+import { LaenderUebersichtComponent } from "./laender-uebersicht/laender-uebersicht.component";
 
 @NgModule({
-	declarations: [],
-	imports: [CommonModule, LaenderRoutingModule],
+	declarations: [LaenderUebersichtComponent],
+	imports: [CommonModule, LaenderRoutingModule, GoogleChartsModule],
 })
 export class LaenderModule {}
