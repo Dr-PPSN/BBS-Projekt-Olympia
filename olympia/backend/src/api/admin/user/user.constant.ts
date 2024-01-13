80ea84553a98950e2dc37f6a4e1c5ce1fc2d4122
Robert <robert.goepfert1999@gmail.com>
2024-01-13 10:15:43 +0100

    Auto stash before merge of "master" and "origin/master"

--- olympia/backend/src/api/admin/user/user.constant.ts
+++ olympia/backend/src/api/admin/user/user.constant.ts
@@ -3,6 +3,7 @@ export namespace Api {
 
 	export const INVITE_USER = "invite-user";
 	export const ADD_USER_DEBUG = "add-user-debug";
+	export const ADD_USER = "add-user";
 	export const DELETE_USER = "deleteuser";
 	export const EDIT_USER = "edituser";
 }
